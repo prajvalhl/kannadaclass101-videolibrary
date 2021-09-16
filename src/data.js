009f94b011a113eb17386c202ea87fef302d036b
Prajval <prajvalhl@gmail.com>
2021-09-16 20:08:00 +0530

    docs: updated dummy data

--- src/data.js
+++ src/data.js
@@ -5,7 +5,7 @@ faker.seed(123);
 export const data = [...Array(50)].map((item) => ({
   id: faker.datatype.uuid(),
   videoName: faker.lorem.sentence(),
-  videoThumbnail: faker.image.image(1280, 720, true),
+  videoThumbnail: faker.image.image(640, 360, true),
   channelName: faker.random.word(),
   channelThumbnail: faker.internet.avatar(),
   year: faker.random.arrayElement([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
